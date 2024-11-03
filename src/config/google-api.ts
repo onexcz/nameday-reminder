eb319319fefcd8e1cabf469044bbbe381e4127e6
Ondřej Exner <ondrej.exner@valatron.com>
2024-11-03 16:12:16 +0100

    google api credentials

--- src/config/google-api.ts
+++ src/config/google-api.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_CONFIG = {
-  CLIENT_ID: 'YOUR_CLIENT_ID',
-  API_KEY: 'AIzaSyAQ587d97XHrHo7R9g-pTSKRHvZ5cFs33E',
+  CLIENT_ID: '1097786243796-11jn3gedqkda4dsi0c256qhh7gpfplgh.apps.googleusercontent.com',
+  API_KEY: 'AIzaSyDP79YAMElviKFBbQlnfOm93-rWrZXpNCY',
   DISCOVERY_DOC: 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
   SCOPES: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/tasks'
 } 
